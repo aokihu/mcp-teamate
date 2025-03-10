7f909a087e26b91bb8c02774023f741f7cb5d9f0
aokihu <aokihu@gmail.com>
2025-03-10 11:22:40 +0800

    移除HTTP API相关的代码，这部分的功能由项目'mcp-teamate-server'来实现

--- src/server/result.ts
+++
@@ -1,6 +0,0 @@
-/**
- * HTTP返回结果
- */
-
-export const success = (data?: any) => (data ? { code: "success", data } : { code: "success" });
-export const error = (message?: string) => (message ? { code: "error", message } : { code: "error" });
